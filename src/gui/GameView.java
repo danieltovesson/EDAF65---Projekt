5c33d66bd7aae00f4b91a9ea67e0bfeaedb7ed9c
dic15ani <dic15ani@student.lu.se>
2018-02-20 11:40:01 +0100

    Fix requested changes

--- src/gui/GameView.java
+++ src/gui/GameView.java
@@ -11,7 +11,6 @@ import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.FlowPane;
@@ -24,10 +23,20 @@ public class GameView extends Application {
 	private String p2Choice;
 	ObservableList<String> data;
 
+	/**
+	 * Creates a window environment for the game and chat function. User inputs
+	 * strings in a TextField object and display them in a ListView object by
+	 * pressing a Button object. Furthermore, three Button objects is accessible
+	 * to play the game. The result of the game is displayed in the ListView
+	 * object.
+	 *
+	 * @param primaryStage
+	 *            the Stage object representing the FlowPane used
+	 */
 	public void start(Stage primaryStage) {
 		primaryStage.setTitle("GameView");
 
-		//Createing Pane
+		// Creating Pane
 		FlowPane root = new FlowPane();
 		root.setHgap(10);
 		root.setVgap(10);
@@ -46,32 +55,32 @@ public class GameView extends Application {
 		chatField.setTranslateX(30);
 		chatField.setTranslateY(450);
 
-		// Defining the Submit button
+		// Defining the send button
 		Button send = new Button("Send");
 		send.setTranslateX(30);
 		send.setTranslateY(450);
 
-		// Defining the Rock button
+		// Defining the rock button
 		Button rockBtn = new Button("Rock");
 		rockBtn.setTranslateX(30);
 		rockBtn.setTranslateY(350);
 
-		// Defining the Paper button
+		// Defining the paper button
 		Button paperBtn = new Button("Paper");
-		// paper.setAlignment(Pos.BOTTOM_CENTER);
 		paperBtn.setTranslateX(30);
 		paperBtn.setTranslateY(350);
 
-		// Defining the Scissors button
+		// Defining the scissors button
 		Button scissorsBtn = new Button("Scissors");
 		scissorsBtn.setTranslateX(30);
 		scissorsBtn.setTranslateY(350);
 
-		// Setting an action for the Submit button
+		// Setting an action for the send button
 		send.setOnAction(new EventHandler<ActionEvent>() {
 
 			@Override
 			public void handle(ActionEvent e) {
+				// Retrieves string from TextField
 				String message = chatField.getText();
 
 				// If empty message --> do not display
@@ -79,15 +88,20 @@ public class GameView extends Application {
 					return;
 				}
 
+				// Adds string to ArrayList object
 				list.add("You: " + message);
-				data = FXCollections.observableList(list);
 
+				// Display the string in the TableView
+				data = FXCollections.observableList(list);
 				view.setItems(data);
 
+				// Make game buttons accessible
 				rockBtn.setDisable(false);
 				paperBtn.setDisable(false);
 				scissorsBtn.setDisable(false);
 
+				// Clear the TextField after string is sent and displayed in
+				// TableView
 				chatField.clear();
 
 			}
@@ -98,18 +112,23 @@ public class GameView extends Application {
 
 			@Override
 			public void handle(ActionEvent e) {
+
+				// String to display when button is pressed
 				String message = "***...waiting for opponent...***";
 
+				// Adds string to ArrayList object
 				list.add(message);
-				data = FXCollections.observableList(list);
 
+				// Display the string in the TableView
+				data = FXCollections.observableList(list);
 				view.setItems(data);
 
-				// Unable to make another move until the opponent has
+				// Make game buttons unaccessible until opponent makes a move
 				rockBtn.setDisable(true);
 				paperBtn.setDisable(true);
 				scissorsBtn.setDisable(true);
 
+				// Player1s choice is set
 				p1Choice = "rock";
 				System.out.println(p1Choice);
 
@@ -122,18 +141,23 @@ public class GameView extends Application {
 
 			@Override
 			public void handle(ActionEvent e) {
+
+				// String to display when button is pressed
 				String message = "***...waiting for opponent...***";
 
+				// Adds string to ArrayList object
 				list.add(message);
-				data = FXCollections.observableList(list);
 
+				// Display the string in the TableView
+				data = FXCollections.observableList(list);
 				view.setItems(data);
 
-				// Unable to make another move until the opponent has
+				// Make game buttons unaccessible until opponent makes a move
 				rockBtn.setDisable(true);
 				paperBtn.setDisable(true);
 				scissorsBtn.setDisable(true);
 
+				// Player1s choice is set
 				p1Choice = "paper";
 				System.out.println(p1Choice);
 
@@ -146,29 +170,36 @@ public class GameView extends Application {
 
 			@Override
 			public void handle(ActionEvent e) {
+
+				// String to display when button is pressed
 				String message = "***...waiting for opponent...***";
 
+				// Adds string to ArrayList object
 				list.add(message);
-				data = FXCollections.observableList(list);
 
+				// Display the string in the TableView
+				data = FXCollections.observableList(list);
 				view.setItems(data);
 
-				// Unable to make another move until the opponent has
+				// Make game buttons unaccessible until opponent makes a move
 				rockBtn.setDisable(true);
 				paperBtn.setDisable(true);
 				scissorsBtn.setDisable(true);
 
+				// Player1s choice is set
 				p1Choice = "scissors";
 				System.out.println(p1Choice);
-
 
 			}
 
 		});
 
+		// Add all components to the FlowPane and make it visible
 		root.getChildren().addAll(send, chatField, rockBtn, paperBtn, scissorsBtn, view);
 		primaryStage.setScene(new Scene(root, 400, 500));
 		primaryStage.show();
+
+		// Make window size fixed
 		primaryStage.setResizable(false);
 
 	}
